9b73234dbd6f75ece60c8b070a4fbb9f751a5c9d
Matheus Vieira <38119742+vssmatheus@users.noreply.github.com>
2021-04-21 09:17:17 -0300

    estrutura

---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Teste</h1>
+        </div>
+    )
+}
+
+export default Dashboard
