626e24a3ea2f04d5ac90d58c85ba3869ac59128b
Matheus Vieira <38119742+vssmatheus@users.noreply.github.com>
2021-04-23 02:30:36 -0300

    navbar ok

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useEffect} from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,24 +7,29 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import logo from '../assets/id_visual/logo.svg';
 import detailNav from '../assets/id_visual/detail_nav.svg';
 import imageUser from '../assets/images/user_image_1.png';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
     },
     container:{
+        display: "flex",
         width: "80%",
         margin: "0 auto",
+        position: "relative"
     },
     navbar: {
         background: "#7A57FD",
         boxShadow: "none",
+        display: "flex",
     },
     logo: {
-        width: "100px",
+        width: "95px",
     },
     my_classes:{
         fontSize: "14px",
@@ -62,6 +67,21 @@ const useStyles = makeStyles((theme) => ({
 
 const Dashboard = () => {
     const classes = useStyles();
+        
+    /* When the user clicks on the button, 
+    toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }      
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(e) {
+        if (!e.target.matches('.dropbtn')) {
+            var myDropdown = document.getElementById("myDropdown");
+            if (myDropdown.classList.contains('show')) {
+                myDropdown.classList.remove('show');
+            }
+        }
+    }
 
     return (
         <div>
@@ -79,9 +99,22 @@ const Dashboard = () => {
                         </Typography>
                         {/* BUTTON CHANGE TO TEACHER MODE*/}
                         <div className="btn_teacher-mode-container">
-                            <button className="btn_teacher-mode">
-                                <Typography className={classes.text_button}>CHANGE TO TEACHER MODE</Typography>
-                            </button>
+                            <Link to="/" className="button_for-desktop">
+                                <button className="btn_teacher-mode">
+                                    <Typography className={classes.text_button}>CHANGE TO TEACHER MODE</Typography>
+                                </button>
+                            </Link>
+                            <div className="dropdown">
+                                <IconButton edge="end" onClick={() => myFunction()} className="dropbtn">
+                                    <ExpandMoreIcon/>
+                                </IconButton>
+                            </div>                   
+                        </div>
+                        <div id="myDropdown" className="dropdown-content">
+                            <Link to="/" className="menu_teacher-mode NonOpaque">
+                                CHANGE TO TEACHER MODE
+                                <ArrowForwardIcon className="arrow-right_teacher-mode"/>
+                            </Link>
                         </div>
                         {/* AVATAR USER */}
                         <IconButton className="icon_button" color="inherit">
